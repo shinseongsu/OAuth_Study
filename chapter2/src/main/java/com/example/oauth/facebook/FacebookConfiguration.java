2b90b341fc484ec47ac5097dfc1719110168dcf0
Shin seongsu <tlstjdtn321@naver.com>
2020-10-18 20:29:03 +0900

    facebookConnection 연습

--- chapter2/src/main/java/com/example/oauth/facebook/FacebookConfiguration.java
+++ chapter2/src/main/java/com/example/oauth/facebook/FacebookConfiguration.java
@@ -1,11 +1,20 @@
 package com.example.oauth.facebook;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.facebook.api.Facebook;
 
 @Configuration
 @EnableSocial
@@ -19,6 +28,22 @@ public class FacebookConfiguration extends SocialConfigurerAdapter {
                 CustomFacebookConnectionFactory(this.properties.getAppId(),
                 this.properties.getAppSecret(),
                 this.properties.getApiVersion());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(Facebook.class)
+    @Scope(value= "request", proxyMode = ScopedProxyMode.INTERFACES)
+    public Facebook facebook(ConnectionRepository repositorry) {
+        Connection<Facebook> connection =
+                repositorry.findPrimaryConnection(Facebook.class);
+        return connection != null ? connection.getApi() : null;
+    }
+
+    @Bean
+    public ConnectController connectContorller(ConnectionFactoryLocator factoryLocator, ConnectionRepository repository) {
+        ConnectController controller = new ConnectController(factoryLocator, repository);
+        controller.setApplicationUrl("http://localhost:8080");
+        return controller;
     }
 
 }
